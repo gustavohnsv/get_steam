b222e0dbf88bc4afa6aed5608d481debd8a0cc80
gustavohnsv <guvi10vieira@gmail.com>
2024-04-28 18:03:57 -0300

    feat: formated date to game played time and friend since date

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,19 +1,27 @@
 import './style.css'
 
 function Friend({ props }) {
+
+    const FormatedDate = (timestamp) => {
+        const date = new Date(timestamp * 1000);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
+    if (!props) return null;  
+
     return (
         <div className="friend">
-            <span> Nome: { props.name } </span>
-            <span> RNome: 
-            { props.realname ? <> { props.realname } </> : <> ∅ </> }
-            </span>
-            <span> Pais: 
-            { props.country ? <> { props.country } </> : <> ∅ </> }
-            </span>
-            <a href={props.url} target='_blank' rel='noreferrer'> Clique aqui para ir ate o perfil </a>
+            <span> Nome: {props.name} </span>
+            <span> RNome: {props.realname ? <> {props.realname} </> : <> ∅ </>} </span>
+            <span> Pais: {props.country ? <> {props.country} </> : <> ∅ </>} </span>
+            <span> Amigos desde {FormatedDate(props.friend_since)} </span>
+            <a href={props.url} target='_blank' rel='noreferrer'> Clique aqui para ir até o perfil </a>
             <img alt={props.name} src={props.avatar}/>
         </div>
-    )
+    );
 }
 
 export default Friend;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,27 @@
 import './style.css'
 
 function Game({ props }) {
+    if (!props) return null;
+
+    function getPlaytime(time) {
+        const totalTime = time;
+        const days = Math.floor(totalTime / 1440);
+        const hours = Math.floor((totalTime % 1440) / 60);
+        const minutes = totalTime % 60;
+        return (
+            <span> Tempo total: 
+            { days > 1 && ` ${days} dias,` }
+            { hours > 1 && ` ${hours} horas e` }
+            { minutes > 1 ? ` ${minutes} minutos` : ' Nunca jogado'} 
+            </span>
+        )
+    }
+
     return (
         <div className="game">
             <div className='game-left'>
                 <strong> { props.name } </strong>
+                { getPlaytime(props.total_playtime) }
                 <span> Tipo: { props.type } </span>
                 <span> Preço: { props.price ? props.price : 'Gratuito' } </span>
                 { props.price && (
@@ -20,7 +37,7 @@ function Game({ props }) {
                         <span> { props.rating_desc } </span>
                     </>
                 )}
-                <span> Website: { props.website ? <a href={ props.website } target='_blank'> { props.website } </a> : 'Não possui' } </span>
+                <span> Website: { props.website ? <a href={ props.website } target='_blank' rel='noreferrer'> { props.website } </a> : 'Não possui' } </span>
             </div>
             <div className='game-right'>
                 <img alt={props.name} src={props.header_image}/>
